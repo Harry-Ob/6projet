d5b6ce6c48bb726f1612093140a41e717d067819
Harry <harry.obertan80@gmail.com>
2024-03-07 17:16:57 +0100

    Script.js: add functions functions=> start() to launch projetch         =>buildElement() to build div gallery         =>addAttributesImg() as his name mention         =>addAttributesFig() as his name mention

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -11,59 +11,98 @@ const apii_works = api + "works";
 // rep.response => shows the body 
 // rep.status => shows the code returned by fonction 
 
-(async () => {
-    try {
-      const response = await fetch(apii);   
-      console.log('HTTP status code:', response.status);
-      // I need to wait for the result of the promise to do something with it otherwise my object isnt really defined 
-      varr = await response.json() ;
-      console.log(varr);
-      console.log(varr.length) ;
-      console.log(varr[0].id);
-      console.log(varr[0].title)
+start(); 
 
-      console.log(varr[0].category.name);
-      console.log(varr[3].category.name); 
+ async function start() {
+  try {
+    const response = await fetch(apii);
+    console.log('HTTP status code:', response.status);
+    // I need to wait for the result of the promise to do something with it otherwise my object isnt really defined 
+    varr = await response.json();
+    console.log(varr);
+    console.log(varr.length);
+    // console.log(varr[0].id);
+    // console.log(varr[0].title)
 
-      console.log(varr[0].categoryId); 
-      console.log(varr[3].categoryId); 
+    // console.log(varr[0].category.name);
+    // console.log(varr[3].category.name);
 
-      // .title => figcaption and alt 
-      // .imageUrl => src 
-      doc =  document.createElement("figure"); 
-      doc.append(document.createElement("img")); 
-      doc.append(document.createElement("figcaption")); 
-      
-      console.log(buildElements(1,["img"]));
+    // console.log(varr[0].categoryId);
+    // console.log(varr[3].categoryId);
 
-      console.log(doc);
-      mott = "bonjour"; 
-      console.log(mott.length);
-      
+    // .title => figcaption and alt 
+    // .imageUrl => src 
+    // doc =  document.createElement("figure"); 
+    // doc.append(document.createElement("img")); 
+    // doc.append(document.createElement("figcaption")); 
 
-
-
-// cree des elements html / createElement ou innerElement / AppendElement / addEventListeners 
-// 
-
-    //  So we have varr that contains everything we need to work on it. 
-    //  it is needed to filter everything i need/want 
-    // 
-    //   console.log(response.body); 
-    } catch (error) {
-      console.error('Error fetching data:', error);
+    // console.log(buildElement(3, ["figure", "img", "figcaption"], varr));
+    // addAttributes(,varr);
+    //  dans notre cas ici on sait que c'est que pour les img qu'on met  des choses 
+    // ensuite pour mettre une figcaption 
+    DOC = null;
+    tab = ["figure", "img", "figcaption"] ;
+    tab2=[]  
+    for (let i = 0; i < varr.length; i++) {
+      // DOC = buildElement(tab[i], varr[i].imageUrl, varr[i].title);
+      DOC = buildElement(tab,varr[i])
+      console.log(DOC);
+      // array1.setAttributes(array1.querySelector("img"),{src: x.imagegUrl, alt: x.title} ; 
+      // DOC.setAttribute(DOC.querySelector("img"),{src: varr[i].imagegUrl, alt: varr[i].title}) ; 
     }
-  })();
+    console.log(DOC);
+    mott = "bonjour";
+    console.log(mott.length);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
 
 // Function that build one element at a time
-function buildElements(num, array){
-  
-  if ( num == 1 ){
-    chose = document.createElement(array[0]);
-  }else{
+// si je suis un figure je met un ID =>  
+// img => src et alt
+function buildElement(array,array2){
+  // rep = document.getElementById ; 
+  var galleryDiv = document.querySelector('#portfolio .gallery');
+  var figure = document.createElement(array[0]);
+  var imge = document.createElement(array[0]);
+  for (let i = 0 ; i<array.length ; i++ ){
+    chose = document.createElement(array[i]); 
+    if (array[i]=== "img"){
+      addAttributesImg(chose,array2);
+      imge=chose;
+    }
+    if (array[i] === "figure"){
+      addAttributesFig(chose,array2);
+      figure = chose ; 
+    }
+    if (array[i] !== "img" && array[i] !== "figure"){
+      chose = document.createElement(array[i]);
+      chose.textContent = array2.title ;
+      figure.append(imge,chose);
+      galleryDiv.appendChild(figure);
+    }
+    // galleryDiv.appendChild(figure);
+    console.log(galleryDiv); 
+  }
+   
+  //  console.log(array); 
+  //  console.log(array2);
+  // chose = document.createElement(array); 
+  // array2.length>1 ? addAttributes(chose,varr) : chose.setAttributes(array2[0],array3[0]);
+  // if ( num == 1 ){
+  //   chose = document.createElement(array[0]);
+  //   chose.setAttributes()
+  // }else{
+  //   // i==0 ?
+  //   chose = document.createElement(array[0]);
+  //   for (let i=1;  i< array.length; i++ ){
+  //     chose.append(document.createElement(array[i])) ;
+  //     // if array[i] == "img" 
+  //   }
     // cb d'elem a cree => num et quels sont leur noms => dans Array 
     // Ils sont ensuite imbriquer dans le premier element qui a ete donnee 
-  }
+  // }
 
 return chose ; }
 
@@ -71,7 +110,32 @@ return chose ; }
 // array1: possedes tous les elements 
 // array2: possede les attributs de l,elements 
 //  x    : la liste d'arguments de mes attributs 
-function addAttributes(array1, array2, x ){
+
+//  img => src et alt 
+function addAttributes(array1,x){
+  array1.setAttributes(array1.querySelector("img"),{src: x.imagegUrl, alt: x.title})
+  // array1.querySelector("img"),
+}
+
+function addAttributesImg(array1,x){
+  // console.log(array1); 
+  // console.log(x.imageUrl); 
+  // console.log(x.title);
+  array1.setAttribute("src",x.imageUrl); 
+  array1.setAttribute("alt",x.title);
+  // array1.setAttributes(array1.querySelector("img"),{src: x.imagegUrl, alt: x.title})
+// return array1 ; 
+}
+
+function addAttributesFig(array1,x){
+  array1.setAttribute("id",x.categoryId);
+  // console.log(array1); 
+  // console.log(x.categoryId);
+  // array1.setAttributes(array1.querySelector("figure"),{id: x.categoryId})
+// return array1 ; 
+}
+
+function add2page(){
 
 }
 
