b885ba73ca398c72021a01e77532762ed846a860
Harry <harry.obertan80@gmail.com>
2024-03-16 18:14:43 +0100

    script.js: refacto code and add functions     refacto => deleting some functions to make better one,                easier to read and to modify     functions => build_filter : to build the filter div and the button inside the div                 build_work : used instead of using buildElements(array,array2)                 buildElement : named after buildElements who doesn't do what it says                 PS : buildElements is now build_work and should be removed in the coming updates

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -8,95 +8,88 @@ const apii_works = api + "works";
 // rep.status => shows the code returned by fonction 
 
 start(); 
-build_work();
-build_filter();
-//  add_button(); => add_category/ event 
-filter_button(); 
 
 
-(async () => {
-  try {
-    const response = await fetch(apii_category);   
-    console.log('HTTP status code:', response.status);
-    // I need to wait for the result of the promise to do something with it otherwise my object isnt really defined 
-    varr = await response.json() ;
-    buffer = [{ id:0, name: 'Tous'}] ;
-    buffer = buffer.concat(varr); 
-    
-  } catch (error) {
-    console.error('Error fetching data:', error);
+
+
+function build_filter(ab){
+  let my_div = buildElement("div");
+  my_div.classList.add("button-container")
+  // my_div.addclass 
+  console.log(ab); 
+  console.log(ab.length);
+  for (let i = 0; i<ab.length ; i++){
+    my_button = buildElement("button");
+    my_button.classList.add("button-filter");
+    my_button.id = ab[i].id;
+    my_button.textContent = ab[i].name ;
+    my_button.addEventListener("click", f1);
+    my_div.append(my_button); 
+
   }
-})();
+  console.log(my_div);
+   tes = document.getElementById("portfolio");
+   test = tes.querySelector("h2");
+   test.insertAdjacentElement('afterend',my_div);
 
-function build_filter(){
   // createDiv 
   // add class to div
   // ajouter la class au bouton et ensuite ajouter le bouton a la div 
   // le faire 4 fois car nbr boutons 
   
-  add_button(); 
-  add2page();  
+  // add_button(); 
+  // add2page();  
+}
+function f1(element){
+  console.log(element.target.id); // build_work() / build_work(arg1)
+  element.target.id == 0? build_work() : filter_work() ; //passer un arg avec 
+} ; 
+// function add_button(){
+//   add_category(); 
+// }
+
+function filter_work() {
+  // trier ma varr qui contient mes work 
+  // tout envoyer a build work avec le nvx tableau trier 
+  
+console.log("r a voir");
 }
 
-function add_button(){
-  add_category(); 
-}
  async function start() {
   try {
     const response = await fetch(apii);
-    // console.log('HTTP status code:', response.status);
-    // I need to wait for the result of the promise to do something with it otherwise my object isnt really defined 
     varr = await response.json();
-    // console.log(varr);
-    // console.log(varr.length);
-    // console.log(varr[0].id);
-    // console.log(varr[0].title)
+    const response_cat = await fetch(apii_category);
+    varr_cat = await response_cat.json();
+    buffer = [{ id:0, name: 'Tous'}] ;
+    buffer = buffer.concat(varr_cat); 
+    // tab = ["figure", "img", "figcaption"] ;
+    // for (let i = 0; i < varr.length; i++) {
+    //   buildElement(tab,varr[i])
+    // }
+    build_work(varr);  
+    console.log(buffer);
+    build_filter(buffer);
+//   add_button(); => add_category/ event 
+// filter_button(); 
 
-    // console.log(varr[0].category.name);
-    // console.log(varr[3].category.name);
-
-    // console.log(varr[0].categoryId);
-    // console.log(varr[3].categoryId);
-
-    // .title => figcaption and alt 
-    // .imageUrl => src 
-    // doc =  document.createElement("figure"); 
-    // doc.append(document.createElement("img")); 
-    // doc.append(document.createElement("figcaption")); 
-
-    // console.log(buildElement(3, ["figure", "img", "figcaption"], varr));
-    // addAttributes(,varr);
-    //  dans notre cas ici on sait que c'est que pour les img qu'on met  des choses 
-    // ensuite pour mettre une figcaption 
-    // DOC = null;
-    
-    tab = ["figure", "img", "figcaption"] ;
-    // tab2=[]  
-    for (let i = 0; i < varr.length; i++) {
-      // DOC = buildElement(tab[i], varr[i].imageUrl, varr[i].title);
-      // DOC = 
-      buildElement(tab,varr[i])
-      // console.log(DOC);
-      // array1.setAttributes(array1.querySelector("img"),{src: x.imagegUrl, alt: x.title} ; 
-      // DOC.setAttribute(DOC.querySelector("img"),{src: varr[i].imagegUrl, alt: varr[i].title}) ; 
-    }
-    // console.log(DOC);
-    // mott = "bonjour";
-    // console.log(mott.length);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 }
+// function build_filter(ab){
+    
 
+// }
 // Function that build one element at a time
 // si je suis un figure je met un ID =>  
 // img => src et alt
 // pas facilement maintenable 
 //  a redecouper  
 // trop generique une fonction pour ce que tu veux faire 
-DOC = null 
-DOC = document.getElementById(maison); 
-function buildElement(array,array2){
+// DOC = null 
+// DOC = document.getElementById(maison); 
+function buildElements(array,array2){
   //  faire du dure et pas comme ca 
   //  tu peut changer certaines chose mais closer to what u wanna build
   var galleryDiv = document.querySelector('#portfolio .gallery');
@@ -109,7 +102,7 @@ function buildElement(array,array2){
       imge=chose;
     }
     if (array[i] === "figure"){
-      addAttributesFig(chose,array2);
+      add_attributes_fig(chose,array2);
       figure = chose ; 
     }
     if (array[i] !== "img" && array[i] !== "figure"){
@@ -120,48 +113,67 @@ function buildElement(array,array2){
     }
   }
    
-  //  console.log(array); 
-  //  console.log(array2);
-  // chose = document.createElement(array); 
-  // array2.length>1 ? addAttributes(chose,varr) : chose.setAttributes(array2[0],array3[0]);
-  // if ( num == 1 ){
-  //   chose = document.createElement(array[0]);
-  //   chose.setAttributes()
-  // }else{
-  //   // i==0 ?
-  //   chose = document.createElement(array[0]);
-  //   for (let i=1;  i< array.length; i++ ){
-  //     chose.append(document.createElement(array[i])) ;
-  //     // if array[i] == "img" 
-  //   }
-    // cb d'elem a cree => num et quels sont leur noms => dans Array 
-    // Ils sont ensuite imbriquer dans le premier element qui a ete donnee 
-  // }
 
 return chose ; }
 
+function buildElement(x){
+  return document.createElement(x);
+}
+// build_work() 
+function build_work(varr){
+  // var global qui contient le contenue de mon work => fait avec fetch 
+  // varr !== undefined ? build-other
+
+
+
+  var galleryDiv = document.querySelector('#portfolio .gallery');
+  const res = []
+  for (let i=0; i<varr.length ; i++ ) {
+    img = buildElement("img") ;
+    // console.log(img);
+    // console.log(varr[i]);
+    img.setAttribute("src",varr[i].imageUrl);
+    img.setAttribute("alt",varr[i].title);
+    figure = buildElement("figure") ;
+    figure.setAttribute("id",varr[i].categoryId);
+    figcaption = buildElement("figcaption") ; 
+    figcaption.textContent = varr[i].title ; 
+    figure.append(img,figcaption);
+    res.push(figure);
+    // galleryDiv.appendChild(figure); // ici voir comment changer cette fonction 
+  }
+
+  galleryDiv.append(...res);// look for the spread operator 
+
+  //  var = 1 ; 
+  //  var = buildElement(); 
+  // put then one into another 
+  // afficherpage() ;
+}
 // Function that build gives attributes to the element given 
 // array1: possedes tous les elements 
 // array2: possede les attributs de l,elements 
 //  x    : la liste d'arguments de mes attributs 
 
 //  img => src et alt 
-function addAttributes(array1,x){
-  array1.setAttributes(array1.querySelector("img"),{src: x.imagegUrl, alt: x.title})
-}
+// function add_attributes_img(array1,x){
+//   array1.setAttributes(array1.querySelector("img"),{src: x.imagegUrl, alt: x.title})
+// }
 
-function addAttributesImg(array1,x){
-  array1.setAttribute("src",x.imageUrl); 
-  array1.setAttribute("alt",x.title);
-}
+// function addAttributesImg(array1,x){
+//   array1.setAttribute("src",x.imageUrl); 
+//   array1.setAttribute("alt",x.title);
+// }
 
-function addAttributesFig(array1,x){
-  array1.setAttribute("id",x.categoryId);
-}
+// function add_attributes_fig(array1,x){
+//   array1.setAttribute("id",x.categoryId);
+// }
 
-function add2page(){
 
-}
+// elle add b to a => franglais a changer 
+// function add2page(a,b){
+//  a.append(b); 
+// }
 
 
 // Function who takes all my categories and build them 
