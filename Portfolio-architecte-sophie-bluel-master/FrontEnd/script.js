47c4a2c33308b5d4a47ed51fcda8f5c6f6c32ea9
Harry <harry.obertan80@gmail.com>
2024-05-11 11:08:37 +0200

    script.js: functions for modale and admin elements     category_modale()=> make the category available for modal

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -6,61 +6,75 @@ const apii_works = api + "works";
 let all_work_elements;
 let all_categories;
 let work_done = 0;
+let work ; 
 
 const gallery_div = document.querySelector('#portfolio .gallery');
 const log_element = document.getElementById("log_element");
-const div_element = document.getElementById("pro");
 let elem_active = [] ; 
 
+let div_element = document.getElementById("pro");
+
+const admin_elements = document.getElementsByClassName("admin-element");
+let id_img ; 
+
+
+function display_on(element){
+element.style ="";
+}
+
+function display_off(element){
+element.style="display:none"
+}
+
+
+
+// activer le button tous par defaut 
 start();
-// test() ; 
-admin_elem();
-// admin_elem(); 
 
-// verifie localSto pour elem admin ou non 
-// test();
-// log_out();  
+connexion() ? admin_elem_on() : admin_elem_off();
 
+function connexion (){
+  return window.localStorage.length > 0 ;
+}
 function test() {
   if (window.localStorage.length > 0)
     admin_elem();
-  // ffsdfdaddd
-  //  aded  adasddddd? admin_elem() : console.log("Ici c'est vide donc tu viens d'arriver");
 }
 
-// if non connect => containerFiltres.style = "display:none"; 
-// maintenant le soucis c'est que mon login/logout fonctionne pas tout le temps car pas de a href donc a noter 
 
-function admin_elem() {
-  // log_element.addEventListener("click",log_);
+function admin_elem_off(){
+  Array.from(admin_elements).forEach(elements => display_off(elements));
 
+}
+// enlever la partit avec DOMCONTENTLOADED 
+function admin_elem_on() {
+  Array.from(admin_elements).forEach(elements => display_on(elements));
+  document.addEventListener("DOMContentLoaded",function () {
+    button_filter_out();
+  });
 
-  //  there we need to have the admin elem showed
 
   if (log_element.innerHTML == "login") {
     if (window.localStorage.length > 0) {
       log_element.innerHTML = "logout";
-      // afficher admin elemenmt 
-      //  => le p 
-      //  => le header 
-      //  => le modifier 
+
       log_element.addEventListener("click", log_out());
     }
 
   } else {
-    // log_element.addEventListener("click",log_in());
     log_element.addEventListener("click", (event) => {
       event.preventDefault();
       window.location.href = "login.html";
     });
   }
-  // log_element == "login" ? log_element.innerHTML="logout" : console.log(window.localStorage.length==NaN);
-  // my_button.addEventListener("click", button_filter); 
+
 }
 
+//  soucis dans cette fonction a gerer 
 function log_() {
   console.log(log_element.innerHTML);
-  log_element.innerHTML=="login" ? log_in() : log_out() ;
+  tss =  log_element.innerHTML ;
+  tss == "login" ? log_in() : log_out() ;
 }
 
 function log_in() {
@@ -68,11 +82,7 @@ function log_in() {
 }
 
 function log_out() {
-
-  // + retirer les elem admin
-  // .header-admin => display none 
-  // elem sur mes projet dehors 
-  // logout redeviens login
+  // console.log("ici c la street");
   log_element.innerHTML = "login";
   window.localStorage.removeItem("user_token");
   window.localStorage.removeItem("user_id");
@@ -80,16 +90,7 @@ function log_out() {
 
   log_element.innerHTML = "logout";
 
-
 }
-
-// function log_in(){
-//   // <a href="http://"></a>
-//   // log_element.innerHTML+='<a href="projet/6projet/Portfolio-architecte-sophie-bluel-master/FrontEnd/login.html"></a>'
-//   // log_element.addEventListener("click",log_out())
-//   window.location.href="login.html";
-//   console.log("here we are");
-// }
 
 
 // all function and the code 
@@ -104,10 +105,13 @@ async function start() {
     temp_category = await result_category.json();
     buffer_category = [{ id: 0, name: 'Tous' }];
     buffer_category = buffer_category.concat(temp_category);
+    // new function for category into modal 
+    category_modale(temp_category);
     all_categories = buffer_category;
     all_work_elements = temp_work;
     build_work(temp_work);
-    build_filter(buffer_category);
+    if ( !connexion() )
+      build_filter(buffer_category);
 
   } catch (error) {
     console.error('Error fetching data:', error);
@@ -118,18 +122,27 @@ async function start() {
 function build_work(work) {
   if (work_done == 1) { gallery_div.innerHTML = ""; }
   const result = []
+ 
   for (let i = 0; i < work.length; i++) {
+  
     img = build_element("img");
     img.setAttribute("src", work[i].imageUrl);
     img.setAttribute("alt", work[i].title);
+    img.setAttribute("id",work[i].id);
     figure = build_element("figure");
     figure.setAttribute("id", work[i].categoryId);
     figcaption = build_element("figcaption");
     figcaption.textContent = work[i].title;
+    toto(img,figure,figcaption);
     figure.append(img, figcaption);
+    
     result.push(figure);
+   
   }
+  
   gallery_div.append(...result);// look for the spread operator 
+  // build_modale_gallery(gallery_div);
+  // work = result;
   work_done = 1;
 }
 
@@ -145,8 +158,6 @@ function button_filter(element) {
   if( elem_active.length > 0) 
     f1() ;
   varr.style = "background-color:#1D6154; color:white";
-  // elem_active.length > 0 ? f1() : varr.style = "background-color:#1D6154; color:white ";; 
-  // varr.style = "background-color:#1D6154; color:white ";
   elem_active.push(varr); 
   console.log(varr);
   element.target.id == 0 ? build_work(all_work_elements) : filter_work(element.target.id); //passer un arg avec 
@@ -154,7 +165,6 @@ function button_filter(element) {
 
 function f1(){
   x = elem_active.pop() ; 
-  // console.log(x);
   x.style = "" ;
 }
 function filter_work(id) {
@@ -163,31 +173,35 @@ function filter_work(id) {
 
 function build_filter(all_filter_elements) {
   let my_div = build_element("div");
-  my_div.classList.add("button-container")
+  my_div.classList.add("button-container");
+  my_div.setAttribute("id","button-container");
   for (let i = 0; i < all_filter_elements.length; i++) {
     my_button = build_element("button");
     my_button.classList.add("button-filter");
     my_button.id = all_filter_elements[i].id;
     my_button.textContent = all_filter_elements[i].name;
+    if (i == 0 ){
+      my_button.style = "background-color:#1D6154; color:white";
+      elem_active.push(my_button);
+    }
     my_button.addEventListener("click", button_filter);
     my_div.append(my_button);
 
   }
   // ici a changer => nom de var a trouver et mettre ici 
   portfolio_element = document.getElementById("portfolio");
-  // portfolio_element.insertAdjacentElement('afterend',my_div);
-  // h2_element = portfolio_element.querySelector("h2");
-  // h2_element.insertAdjacentElement('afterend', my_div);
-  // span_element = portfolio_element.querySelector("span");
-  // span_element.insertAdjacentElement('afterend', my_div);
-  // div_element = portfolio_element.querySelector("div") ;
+  div_element = portfolio_element.querySelector("div");
   div_element.insertAdjacentElement('afterend', my_div);
 }
 
 
-// here because u left the other page with a redirection 
-function log_out() {
-  // empty local sto 
-  // rediriger vers login ou rester la ou tu es et supp elem admin 
-  // 
-}
+
+
+
+function button_filter_out(button_container_element){
+  var button_container_element = document.getElementById("button-container");
+  if(button_container_element)
+    display_off(button_container_element);
+  else
+    console.log("nothing to be seen there");
+} 
