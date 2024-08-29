65872eab662f7606a79d58c3574beb2e1412e39e
Harry <harry.obertan80@gmail.com>
2024-08-29 08:50:51 +0200

    script-modal.js: var declared and functions added var => global var for the code to work functions =>     => form_complete     => form_preview     => category_modale     => toto =>  TO REFACTOR         => img of the modal taken during and cloned during build     => build_trash_icon_elem     => add/remove image in the database.

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script-modal.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script-modal.js
@@ -1,5 +1,4 @@
-// const { debug } = require("console");
-
+// all var 
 let modifier_element = document.getElementById("open_modale");
 let modale_container_element = document.getElementById("modale-container");
 let modale_show_element = document.getElementById("modale-show");
@@ -11,86 +10,171 @@ let left_arrow = document.getElementById("previous-modale");
 let modale_gallery_element = gallery_div;
 let modale_gallery_img = document.getElementById("modale-show-main");
 
-// const dd
+const url_database = "http://localhost:5678/api/works";
+const input_file = document.getElementById("input-file");
+const input_title = document.getElementById("input-title");
+const input_category = document.getElementById("input-category");
+// a utiliser pour avoir notre preview de l'img
+const input_img = document.getElementById("input-img");
+const input_img_data = {
+    // id: "",
+    title: "",
+    src: "",
+    category_id: "",
+    user_id: 0
+};
+const form_data = new FormData(); 
 
-
+// main fonction usage 
 modale();
-// modale_gallery(modale_gallery_element);
 
+
+
+// function declaration 
 function modale() {
     open_modale();
     close_modale();
     next_modale();
     previous_modale();
-    console.log(modale_gallery_img);
-    // console.log()
+    form_preview();
 }
-function toto(img,figure,figcaption){
-    // console.log(figure,img,figcaption);
+
+function form_complete(img) {
+
+    const form_add = document.getElementById("input-form");
+    form_add.addEventListener("submit",elem => {
+        elem.preventDefault();
+        form_data.append("img",img);
+        form_data.append("categoryId",input_img_data.category_id);
+        
+    });
+    add_to_database(form_data);
+    // when form is complete je lance add_to_database()
+}
+
+
+
+function form_preview() {
+    // img preview 
+    let file;
+    input_file.addEventListener("change", () => {
+        file = input_file.files[0];
+        // if file is good read it 
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (elem) {
+                input_img_data.src = elem.target.result;
+            };
+            reader.readAsDataURL(file);
+            f_name = file.name  ; 
+            f_name = f_name.slice(0,f_name.lastIndexOf('.')) ; 
+            input_title.value = f_name ; 
+            if ( input_img_data.category_id != "" && input_title.value != "" ){
+                document.getElementById("submit-form-add-img").className = "submit-button" ; 
+                form_complete(file);
+            }
+        }
+    });
+    input_category.addEventListener("change", (elem) => {
+        // console.log(elem.target);
+        input_img_data.category_id = elem.target.value;
+        if ( input_img_data.category_id != "" && input_title.value != "" ){
+            document.getElementById("submit-form-add-img").className = "submit-button" ; 
+            form_complete(file);
+        }
+    });
+
+    
+    
+}
+function category_modale(elem) {
+    for (i = 0; i < elem.length; i++) {
+        option_element = build_element("option");
+        option_element.value = elem[i].id;
+        option_element.text = elem[i].name;
+        input_category.append(option_element);
+    }
+}
+// toto is called into script.js and build the gallery at the same time
+// that build_work is called
+function toto(img, figure, figcaption) {
     c_figure = figure.cloneNode();
     c_img = img.cloneNode();
     c_figcaption = figcaption.cloneNode();
-    c_figure.append(build_trash_icon_element(),c_img,c_figcaption);
+    c_figcaption.innerHTML = figcaption.innerHTML;
+    c_figcaption.style = "display:none";
+    c_figure.append(build_trash_icon_element(), c_img, c_figcaption);
     modale_gallery_img.append(c_figure);
 }
 
-function build_trash_icon_element(){    
-const span_element = build_element("span");
-const i_element = build_element("i");
-i_element.classList.add("fa-solid", "fa-trash-can");
-span_element.append(i_element);
-    return span_element; 
+function build_trash_icon_element() {
+    const span_element = build_element("span");
+    const i_element = build_element("i");
+    i_element.classList.add("fa-solid", "fa-trash-can");
+    span_element.addEventListener("click", (elem) => {
+        parent_elem = elem.target.parentElement;
+        parent_elem = parent_elem.parentElement;
+        img = parent_elem.querySelector("img");
+        remove_from_database(img.id);
+    });
+    //  tu trouves ton id pour delete avec ton queryselector du parent de ton elem 
+    span_element.append(i_element);
+    return span_element;
 }
 
-function build_modale_gallery(gallery) {
-    const div_element = build_element("div");
 
-    // const figure
+function add_to_database(formdata) {
+    // id_img +=1; rajouter au bon endroit 
     
-    // console.log(gallery[0]);
-    // se = gallery.children;
-    //     for (let i=0; i<se.length; i++){
-    //         // console.log(se.item(i).insertAdjacentElement('afterbegin',span_element)); 
-    //         console.log(se.item(i).innerHTML);
-    //         console.log(se.item(i));
-
-    //         // add_span_modale_gallery(se[i],span_element);
-    //         // se[i].insertAdjacentElement('afterend', span_element);
-    //         // console.log(se[i]);
-    //     }
-    // console.log(se);
-    // modale_gallery_img.append(...se);
-    // console.log(se[1].insertAdjacentElement('afterbegin', span_element));
-    // se[2].insertAdjacentElement('afterbegin', span_element);
-    // se[3].insertAdjacentElement('afterbegin', span_element);
-    // console.log(se);
-    // for (let i = 0; i < gallery.children.length; i++) {
-    //     se[i].insertAdjacentElement('afterbegin', span_element);
-    // }
-
+    return console.log("DONE-add");
+exit 
 }
 
-function add_span_modale_gallery(elem,span){
-    elem.insertAdjacentElement('beforebegin',span);
-    // console.log(elem);
-    // console.log(span);
+function remove_from_database(id) {
+    // code non complet
+    // ici pour faire le remove avec un appel bien concluant on peut attendre de sortir ou de revenir en arriere 
+    // pour faire l'appel complet du fetch 
+    // tu delete 
+    // tu rebuild ta gallery 
+    // et ta modale 
 
+    // id_img -=1; a mettre au bon endroit 
+    return console.log("DONE-delete");
+    const user_token = window.localStorage.getItem("user_token");
+    const url_database_delete = url_database + "/" + id;
+    const arg_author = "Bearer " + user_token;
+    const request = {
+        method: "DELETE",
+        headers: {
+            authorization: arg_author,
+            "Content-type": "application/json",
+        },
+        mode: "cors",
+        credential: "same-origin",
+    };
+
+    // verifier quoi faire une fois que l'image est supp de la BD
+    // regarder quel fonction appeler pour tout refaire 
+    // build_work et build_modale_visuals 
+    fetch(url_database_delete, request,).then(start()).then().catch();
+
+}
+function add_span_modale_gallery(elem, span) {
+    elem.insertAdjacentElement('beforebegin', span);
 }
 
 function modale_gallery() {
-    //     console.log("???");
-    //  console.log(gallery_div);
     build_modale_gallery();
 }
+
 //  look why it works like that and not with named function in this case 
 function open_modale() {
     modifier_element.addEventListener("click", () => {
         modale_container_element.style = "display:block";
         modale_show_element.style = "display:flex";
     });
-
-
 }
+
 function close_modale() {
     close_modale_element.addEventListener("click", () => {
         modale_container_element.style = "";
@@ -99,10 +183,6 @@ function close_modale() {
 
 }
 
-function un() {
-    modale_container_element.style = "display:block";
-    modale_show_element.style = "display:flex";
-}
 function next_modale() {
     valider_modale_element.addEventListener("click", () => {
         modale_add_element.style = "display:flex";
@@ -113,11 +193,10 @@ function next_modale() {
         modale_add_element.style = "";
     });
 }
+
 function previous_modale() {
     left_arrow.addEventListener("click", () => {
         modale_add_element.style = "";
         modale_show_element.style = "display:flex";
-
     });
-
 }
